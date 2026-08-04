02c0593902a9957076b7f4f3e15efaeaac96dc51
Viktoriia <ogay_viktory@gmail.com>
2026-08-04 23:37:37 +0900

    one more time fix Modal.tsx

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -4,7 +4,7 @@ import "@/components/ui/Modal.css";
 
 // Иконки
 import PhoneIcon from "@/assets/icons/phone.svg?react";
-import TelegramIcon from "@/assets/icons/Telegram.svg";
+import TelegramIcon from "@/assets/icons/Telegram.svg?react";
 import WhatsappIcon from "@/assets/icons/WhatsApp.svg?react";
 import Down from "@/assets/icons/chevron-down.svg?react";
 
